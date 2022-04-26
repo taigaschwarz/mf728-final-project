4ef3d863c998f2069b7d8656313e59d71809aab9
Caleb <Caleb@dhcp-wifi-8021x-155-41-59-150.bu.edu>
2022-04-26 17:15:34 -0400

    modify hedging.py

--- hedging.py
+++ hedging.py
@@ -11,34 +11,11 @@ import matplotlib.pyplot as plt
 import scipy
 from scipy import linalg
 from scipy.optimize import minimize
-
 import os
 os.chdir('/Users/Caleb/Documents/Classes/Boston/Spring 2022/728/FinalProject')
-
 from piecewise_const_forward import *
 
 
-# What curves look like naturally
-swap_df = pd.read_csv('SOFR_swap.csv')
-sofr_swaps = np.array(swap_df.iloc[0, :])[3:]  # starting at the 1 year swap
-
-# sofr_swaps[0] = 2.4217
-# sofr_swaps[1] = 2.3912
-
-
-terms = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20, 25, 30])
-zero_rates, constant_forwards = piecewise_const_fwd(sofr_swaps, terms, 0.5, 0.5)
-tenors = np.arange(terms[0] - 0.5, terms[-1] + 0.5, 0.5)
-
-plt.plot(terms, constant_forwards, label='forward curve')
-plt.plot(tenors, zero_rates, label='zero curve')
-plt.xlabel('time (years)')
-plt.title('Yield Curves by Raw Interpolation Method (as of 2022-03)')
-plt.legend()
-plt.show()
-
-
-# asdf = piecewise_const_fwd(bull_steepener(4, 0.01, 5, sofr_swaps), terms, 0.5, 0.5)[0]
 
 
 # bumps up the ith term rates
@@ -47,14 +24,10 @@ def bump(i, diff, curve):
     asdf[i] = asdf[i] +  diff
     return asdf
 
-
-
-
 # Bull steepener. For bear steepener set negative slope
 def triangle(i, fixed_height, apex, curve):
     # slope is in terms of basis points
             
-    
     shift = np.zeros(15)
     if i == 0:
         shift[0] = apex
@@ -70,15 +43,6 @@ def triangle(i, fixed_height, apex, curve):
         
     return curve + shift
 
-
-
-triangle(11, 0.1, 0.2, sofr_swaps )
-triangle(10, 0.1, 0.2, sofr_swaps )
-triangle(9, 0.1, 0.2, sofr_swaps )
-triangle(8, 0.1, 0.2, sofr_swaps )
-triangle(7, 0.1, 0.2, sofr_swaps )
-
-
 # Gets a vector of prices of each of the bootstrapping instruments (the sofr_swaps rates)
 def instrument_prices(terms, rates):
     prices = np.zeros(len(terms))
@@ -87,67 +51,26 @@ def instrument_prices(terms, rates):
     return prices
 
 
-
-# See the effects of bumps
-bp = 0.01
-for i in range(0, 15):
-    sofr_swaps = np.array(swap_df.iloc[0, :])[3:]
-    sofr_swaps = bump(i, bp, sofr_swaps)
-    zero_rates, constant_forwards = piecewise_const_fwd(sofr_swaps, terms, 0.5, 0.5)
-    plt.plot(terms, constant_forwards, label='forward curve')
-    plt.plot(tenors, zero_rates, label='zero curve')
-    plt.xlabel('time (years)')
-    plt.title('Effect of Bumps on Yield Curves by Raw Interpolation Method (as of 2022-03)')
-    plt.legend()
-    plt.show()
-    
-    
-# see the effects of triangle
-bp = 0.01
-for i in range(0, 15):
-    sofr_swaps = np.array(swap_df.iloc[0, :])[3:]
-    sofr_swaps = triangle(i, 0.01, 0.05, sofr_swaps )
-    zero_rates, constant_forwards = piecewise_const_fwd(sofr_swaps, terms, 0.5, 0.5)
-    plt.plot(tenors, zero_rates, label='zero curve')
-    plt.xlabel('time (years)')
-    plt.title('Effect of Steepeners on Yield Curves by Raw Interpolation Method (as of 2022-03)')
-    plt.legend()
-    plt.show()
-    
-# see the effects of pure bumps
-bp = 0.1
-for i in range(0, 15):
-    print(i)
-    sofr_swaps = np.repeat(1, 15)
-    sofr_swaps = bump(i, bp, sofr_swaps)
-    plt.plot(terms, sofr_swaps, label='curve')
-    plt.xlabel('time (years)')
-    plt.title('Visualization of Effect of Steepener')
-    plt.legend()
-    plt.show()
-    
-    
-# see the effects of pure triangle
-bp = 0.1
-for i in range(0, 15):
-    print(i)
-    sofr_swaps = np.repeat(1, 15)
-    sofr_swaps = triangle(i, 0.1, 0.2, sofr_swaps)
-    plt.plot(terms, sofr_swaps, label='curve')
-    plt.xlabel('time (years)')
-    plt.title('Visualization of Effect of Steepener')
-    plt.legend()
-    plt.show()
+# What curves look like naturally
+swap_df = pd.read_csv('SOFR_swap.csv')
+sofr_swaps = np.array(swap_df.iloc[0, :])[3:]  # starting at the 1 year swap
+terms = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20, 25, 30])
+zero_rates, constant_forwards = piecewise_const_fwd(sofr_swaps, terms, 0.5, 0.5)
+tenors = np.arange(terms[0] - 0.5, terms[-1] + 0.5, 0.5)
 
 
+plt.plot(terms, constant_forwards, label='forward curve')
+plt.plot(tenors, zero_rates, label='zero curve')
+plt.xlabel('time (years)')
+plt.title('Yield Curves by Raw Interpolation Method (as of 2022-03)')
+plt.legend()
+plt.show()
 
 
 
 '''
 STEEPENER
 '''
-
-
 # Creating a steepener by shifting sofr swap rates
 bear_steepener = sofr_swaps.copy()
 for i in range(len(terms) - 2):
@@ -156,16 +79,6 @@ for i in range(len(terms) - 2):
 # getting shifted zero rates and constant forwards with steepened sofr rates
 zero_rates, constant_forwards = piecewise_const_fwd(bear_steepener, terms, 0.5, 0.5)
 
-
-# Visualizing the difference between steepened and non steepened sofr swaps
-plt.plot(terms, bear_steepener, label='bear steepener')
-plt.plot(terms, sofr_swaps, label='sofr_swaps')
-plt.xlabel('time (years)')
-plt.title('bear steepener of sofr swap rates (as of 2022-03)')
-plt.legend()
-plt.show()
-
-
 # Plotting steepened constant and zero rates. Compare these with before steepening (from up there)
 plt.plot(terms, constant_forwards, label='forwards curve')
 plt.plot(tenors, zero_rates, label='zero curve')
@@ -173,10 +86,19 @@ plt.xlabel('time (years)')
 plt.title('zero and forwards after steepening (as of 2022-03)')
 plt.legend()
 plt.show()
-    
+
+
+# Visualizing the difference between steepened and non steepened sofr swaps
+plt.plot(terms, bear_steepener, label='after steepener')
+plt.plot(terms, sofr_swaps, label='before steepener')
+plt.xlabel('time (years)')
+plt.title('bear steepener of sofr swap rates (as of 2022-03)')
+plt.legend()
+plt.show()
+
 
     
-    
+
 # hedging a steepened portfolio. Plot the portfolio changes we need to make like in hagan and west?
 
 # Hedging part PQ = deltaV
@@ -215,8 +137,9 @@ def pvoptim(weights):
 
 # Performing present value matching, and plotting
 new_weights = minimize(pvoptim, np.repeat(1/15, 15),  method='Nelder-Mead', tol=1e-20).x
-plt.plot(terms, init_weights, label='before steepener')
-plt.plot(terms, new_weights, label='after steepener')
+
+# Plot of difference between new weights and initial weights. Increase in low maturity weights
+plt.bar(terms, (new_weights - init_weights) * 15, label='before steepener')
 plt.xlabel('bond maturity (years)')
 plt.title('Portfolio weights after steepening (as of 2022-03)')
 plt.legend()
@@ -245,6 +168,7 @@ plt.title('Portfolio weights after steepening (as of 2022-03)')
 plt.legend()
 plt.show()
 
+
     
 # This hedge does not look too reasonable likely because of the spike in the beginning
 # PQ = deltaV
@@ -258,3 +182,52 @@ plt.plot(np.arange(1, 61), port_mat.T)
 
 
 
+# Bumps
+# See the effects of bumps
+bp = 0.01
+for i in range(0, 15):
+    sofr_swaps = np.array(swap_df.iloc[0, :])[3:]
+    sofr_swaps = bump(i, bp, sofr_swaps)
+    zero_rates, constant_forwards = piecewise_const_fwd(sofr_swaps, terms, 0.5, 0.5)
+    plt.plot(terms, constant_forwards, label='forward curve')
+    plt.plot(tenors, zero_rates, label='zero curve')
+    plt.xlabel('time (years)')
+    plt.title('Effect of Bumps on Yield Curves by Raw Interpolation Method (as of 2022-03)')
+    plt.legend()
+    plt.show()
+    
+# see the effects of triangle
+bp = 0.01
+for i in range(0, 15):
+    sofr_swaps = np.array(swap_df.iloc[0, :])[3:]
+    sofr_swaps = triangle(i, 0.01, 0.05, sofr_swaps )
+    zero_rates, constant_forwards = piecewise_const_fwd(sofr_swaps, terms, 0.5, 0.5)
+    plt.plot(tenors, zero_rates, label='zero curve')
+    plt.xlabel('time (years)')
+    plt.title('Effect of Steepeners on Yield Curves by Raw Interpolation Method (as of 2022-03)')
+    plt.legend()
+    plt.show()
+    
+# see the effects of pure bumps
+bp = 0.1
+for i in range(0, 15):
+    print(i)
+    sofr_swaps = np.repeat(1, 15)
+    sofr_swaps = bump(i, bp, sofr_swaps)
+    plt.plot(terms, sofr_swaps, label='curve')
+    plt.xlabel('time (years)')
+    plt.title('Visualization of Effect of Steepener')
+    plt.legend()
+    plt.show()
+    
+# see the effects of pure triangle
+bp = 0.1
+for i in range(0, 15):
+    print(i)
+    sofr_swaps = np.repeat(1, 15)
+    sofr_swaps = triangle(i, 0.1, 0.2, sofr_swaps)
+    plt.plot(terms, sofr_swaps, label='curve')
+    plt.xlabel('time (years)')
+    plt.title('Visualization of Effect of Steepener')
+    plt.legend()
+    plt.show()
