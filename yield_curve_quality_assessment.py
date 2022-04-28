042678b3606df487df21b86101467dbf0a9a791e
Taiga Schwarz <taigaschwarz@nat-wireless-guest-reg-153-51.bu.edu>
2022-04-28 19:51:28 -0400

    negative rates analysis

--- yield_curve_quality_assessment.py
+++ yield_curve_quality_assessment.py
@@ -44,135 +44,136 @@ if __name__ == '__main__':
 
     # ############ test localness of the interpolation method ############
 
-    zero_rates = np.array(zero_rates_df.loc['2022-03'])
-    # terms = [0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30]
-    #
-    # ### piecewise constant forwards
-    # # swaps = np.array(SOFR_swaps_df.loc['2022-02', '1Y':'30Y'])/100
-    # # terms = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20, 25, 30])
-    #
-    # # actual zero curve and forward curve
-    # PCF = Raw_interpolation(terms, zero_rates)
-    # f_rates1, r_rates1 = PCF.raw_interpolation()
-    # # r_rates1, f_rates1 = piecewise_const_fwd(swaps, terms, 0.5, 0.5)
-    #
-    # # bump the inputs
-    # bps = 5
-    # bumped_zero_rates = bumps(zero_rates, bps)
-    # r_bumps1 = []
-    # f_bumps1 = []
-    # for i in range(len(zero_rates)):
-    #     PCF = Raw_interpolation(terms, bumped_zero_rates[i])
-    #     constant_forwards, r_rates = PCF.raw_interpolation()
-    #     r_bumps1.append(r_rates)
-    #     f_bumps1.append(constant_forwards)
-    # r_bumps1 = np.array(r_bumps1)
-    # f_bumps1 = np.array(f_bumps1)
-    #
-    # # visualize
-    # xx = np.linspace(0, max(terms), 600)
-    # tenors = np.insert(terms, 0, 0)
-    # for i in range(1,len(zero_rates)):
-    #     plt.figure(figsize=(12, 6))
-    #     plt.subplot(1, 2, 1)
-    #     plt.plot(xx, r_bumps1[i], 'orange', label='w/ bump')
-    #     plt.plot(xx, r_rates1, 'blue', label='w/out bump')
-    #     plt.xlabel('time')
-    #     plt.ylabel('zero rates')
-    #     plt.title('zero curve (piecewise constant forwards)')
-    #     plt.grid(True)
-    #     plt.legend()
-    #     plt.subplot(1, 2, 2)
-    #     plt.plot(tenors, f_bumps1[i], 'orange', label='w/ bump')
-    #     plt.plot(tenors, f_rates1, 'blue', label='w/out bump')
-    #     plt.xlabel('time')
-    #     plt.ylabel('f rates')
-    #     plt.title('instantaneous forward curve (piecewise constant forwards)')
-    #     plt.grid(True)
-    #     plt.legend()
-    #     plt.suptitle(f'input rate {terms[i]}Y bumped by {bps} bps')
-    #     plt.show()
-    #
-    # ### natural cubic spline
-    #
-    # # actual zero curve and forward curve
-    # S_actual = Spline_fitting(terms, zero_rates)
-    # r_rates2, f_rates2 = S_actual.cubic_spline(show_plot=False)
-    #
-    # # bumped rates
-    # bumped_zero_rates = bumps(zero_rates, bps)
-    # r_bumps2 = []
-    # f_bumps2 = []
-    # for i in range(len(zero_rates)):
-    #     S = Spline_fitting(terms, bumped_zero_rates[i])
-    #     zero_rate1, f_rate1 = S.cubic_spline(show_plot=False)
-    #     r_bumps2.append(zero_rate1)
-    #     f_bumps2.append(f_rate1)
-    # r_bumps2 = np.array(r_bumps2)
-    # f_bumps2 = np.array(f_bumps2)
-    #
-    # # visualize
-    # xx = np.linspace(0, max(terms), 600)
-    # for i in range(len(zero_rates)):
-    #     plt.figure(figsize=(12, 6))
-    #     plt.subplot(1, 2, 1)
-    #     plt.plot(xx, r_bumps2[i], 'orange', label='w/ bump')
-    #     plt.plot(xx, r_rates2, 'blue', label='w/out bump')
-    #     plt.xlabel('tenor')
-    #     plt.ylabel('zero rates')
-    #     plt.title('zero curve')
-    #     plt.grid(True)
-    #     plt.legend()
-    #     plt.subplot(1, 2, 2)
-    #     plt.plot(xx, f_bumps2[i], 'orange', label='w/ bump')
-    #     plt.plot(xx, f_rates2, 'blue', label='w/out bump')
-    #     plt.xlabel('time')
-    #     plt.ylabel('f rates')
-    #     plt.title('instantaneous forward curve')
-    #     plt.grid(True)
-    #     plt.legend()
-    #     plt.suptitle(f'input rate {terms[i]}Y bumped by {bps} bps (natural cubic spline)')
-    #     plt.show()
-    #
-    # ### cubic B-spline
-    #
-    # # actual zero curve and forward curve
-    # S_actual = Spline_fitting(terms, zero_rates)
-    # r_rates3, f_rates3 = S_actual.Bspline(show_plot=False)
-    #
-    # # bumped rates
-    # r_bumps3 = []
-    # f_bumps3 = []
-    # for i in range(len(zero_rates)):
-    #     S = Spline_fitting(terms, bumped_zero_rates[i])
-    #     zero_rate3, f_rate3 = S.Bspline(show_plot=False)
-    #     r_bumps3.append(zero_rate3)
-    #     f_bumps3.append(f_rate3)
-    # r_bumps3 = np.array(r_bumps3)
-    # f_bumps3 = np.array(f_bumps3)
-    #
-    # # visualize
-    # xx = np.linspace(0, max(terms), 600)
-    # for i in range(len(zero_rates)):
-    #     plt.figure(figsize=(12, 6))
-    #     plt.subplot(1, 2, 1)
-    #     plt.plot(xx, r_bumps3[i], 'orange', label='w/ bump')
-    #     plt.plot(xx, r_rates3, 'blue', label='w/out bump')
-    #     plt.xlabel('tenor')
-    #     plt.ylabel('zero rates')
-    #     plt.title('zero curve')
-    #     plt.grid(True)
-    #     plt.legend()
-    #     plt.subplot(1, 2, 2)
-    #     plt.plot(xx, f_bumps3[i], 'orange', label='w/ bump')
-    #     plt.plot(xx, f_rates3, 'blue', label='w/out bump')
-    #     plt.xlabel('time')
-    #     plt.ylabel('f rates')
-    #     plt.title('instantaneous forward curve')
-    #     plt.grid(True)
-    #     plt.legend()
-    #     plt.suptitle(f'input rate {terms[i]}Y bumped by {bps} bps (cubic B-spline)')
-    #     plt.show()
+    yr_month = '2022-03'
+    zero_rates = np.array(zero_rates_df.loc[yr_month])
+    terms = [0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30]
+
+    ### piecewise constant forwards
+    # swaps = np.array(SOFR_swaps_df.loc['2022-02', '1Y':'30Y'])/100
+    # terms = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20, 25, 30])
+
+    # actual zero curve and forward curve
+    PCF = Raw_interpolation(terms, zero_rates)
+    f_rates1, r_rates1 = PCF.raw_interpolation()
+    # r_rates1, f_rates1 = piecewise_const_fwd(swaps, terms, 0.5, 0.5)
+
+    # bump the inputs
+    bps = 5
+    bumped_zero_rates = bumps(zero_rates, bps)
+    r_bumps1 = []
+    f_bumps1 = []
+    for i in range(len(zero_rates)):
+        PCF = Raw_interpolation(terms, bumped_zero_rates[i])
+        constant_forwards, r_rates = PCF.raw_interpolation()
+        r_bumps1.append(r_rates)
+        f_bumps1.append(constant_forwards)
+    r_bumps1 = np.array(r_bumps1)
+    f_bumps1 = np.array(f_bumps1)
+
+    # visualize
+    xx = np.linspace(0, max(terms), 600)
+    tenors = np.insert(terms, 0, 0)
+    for i in range(1,len(zero_rates)):
+        plt.figure(figsize=(12, 6))
+        plt.subplot(1, 2, 1)
+        plt.plot(xx, r_bumps1[i], 'orange', label='w/ bump')
+        plt.plot(xx, r_rates1, 'blue', label='w/out bump')
+        plt.xlabel('time')
+        plt.ylabel('zero rates')
+        plt.title('zero curve')
+        plt.grid(True)
+        plt.legend()
+        plt.subplot(1, 2, 2)
+        plt.plot(tenors, f_bumps1[i], 'orange', label='w/ bump')
+        plt.plot(tenors, f_rates1, 'blue', label='w/out bump')
+        plt.xlabel('time')
+        plt.ylabel('f rates')
+        plt.title('instantaneous forward curve')
+        plt.grid(True)
+        plt.legend()
+        plt.suptitle(f'input rate {tenors[i]}Y bumped by {bps} bps (raw interpolation) - {yr_month}')
+        plt.show()
+
+    ### natural cubic spline
+
+    # actual zero curve and forward curve
+    S_actual = Spline_fitting(terms, zero_rates)
+    r_rates2, f_rates2 = S_actual.cubic_spline(show_plot=False)
+
+    # bumped rates
+    bumped_zero_rates = bumps(zero_rates, bps)
+    r_bumps2 = []
+    f_bumps2 = []
+    for i in range(len(zero_rates)):
+        S = Spline_fitting(terms, bumped_zero_rates[i])
+        zero_rate1, f_rate1 = S.cubic_spline(show_plot=False)
+        r_bumps2.append(zero_rate1)
+        f_bumps2.append(f_rate1)
+    r_bumps2 = np.array(r_bumps2)
+    f_bumps2 = np.array(f_bumps2)
+
+    # visualize
+    xx = np.linspace(0, max(terms), 600)
+    for i in range(len(zero_rates)):
+        plt.figure(figsize=(12, 6))
+        plt.subplot(1, 2, 1)
+        plt.plot(xx, r_bumps2[i], 'orange', label='w/ bump')
+        plt.plot(xx, r_rates2, 'blue', label='w/out bump')
+        plt.xlabel('tenor')
+        plt.ylabel('zero rates')
+        plt.title('zero curve')
+        plt.grid(True)
+        plt.legend()
+        plt.subplot(1, 2, 2)
+        plt.plot(xx, f_bumps2[i], 'orange', label='w/ bump')
+        plt.plot(xx, f_rates2, 'blue', label='w/out bump')
+        plt.xlabel('time')
+        plt.ylabel('f rates')
+        plt.title('instantaneous forward curve')
+        plt.grid(True)
+        plt.legend()
+        plt.suptitle(f'input rate {terms[i]}Y bumped by {bps} bps (natural cubic spline) - {yr_month}')
+        plt.show()
+
+    ### cubic B-spline
+
+    # actual zero curve and forward curve
+    S_actual = Spline_fitting(terms, zero_rates)
+    r_rates3, f_rates3 = S_actual.Bspline(show_plot=False)
+
+    # bumped rates
+    r_bumps3 = []
+    f_bumps3 = []
+    for i in range(len(zero_rates)):
+        S = Spline_fitting(terms, bumped_zero_rates[i])
+        zero_rate3, f_rate3 = S.Bspline(show_plot=False)
+        r_bumps3.append(zero_rate3)
+        f_bumps3.append(f_rate3)
+    r_bumps3 = np.array(r_bumps3)
+    f_bumps3 = np.array(f_bumps3)
+
+    # visualize
+    xx = np.linspace(0, max(terms), 600)
+    for i in range(len(zero_rates)):
+        plt.figure(figsize=(12, 6))
+        plt.subplot(1, 2, 1)
+        plt.plot(xx, r_bumps3[i], 'orange', label='w/ bump')
+        plt.plot(xx, r_rates3, 'blue', label='w/out bump')
+        plt.xlabel('tenor')
+        plt.ylabel('zero rates')
+        plt.title('zero curve')
+        plt.grid(True)
+        plt.legend()
+        plt.subplot(1, 2, 2)
+        plt.plot(xx, f_bumps3[i], 'orange', label='w/ bump')
+        plt.plot(xx, f_rates3, 'blue', label='w/out bump')
+        plt.xlabel('time')
+        plt.ylabel('f rates')
+        plt.title('instantaneous forward curve')
+        plt.grid(True)
+        plt.legend()
+        plt.suptitle(f'input rate {terms[i]}Y bumped by {bps} bps (cubic B-spline) - {yr_month}')
+        plt.show()
 
     # ### monotone convex method (Hagan-West)
     #
